ce7748b397e147652e44a488cba96109a730a6e9
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-19 20:27:37 +0200

    add validation to form

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         const name = e.target.name;
         const type = e.target.type;
 
-        if (type === 'text' || type === 'password ||' || type === 'email') {
+        if (type === 'text' || type === 'password' || type === 'email') {
             const value = e.target.value;
             this.setState({
                 [name]: value,
@@ -41,7 +41,72 @@ class App extends Component {
     };
 
     handleSubmit = e => {
-        e.preventDefault()
+        e.preventDefault();
+
+        const validation = this.formValidation();
+
+        if (validation.correct) {
+            this.setState({
+                username: '',
+                email: '',
+                pass: '',
+                accept: false,
+
+                errors: {
+                    username: false,
+                    email: false,
+                    pass: false,
+                    accept: false,
+                }
+            })
+        } else {
+            this.setState({
+                errors: {
+                    username: !validation.username,
+                    email: !validation.email,
+                    pass: !validation.pass,
+                    accept: !validation.accept,
+                }
+            })
+        }
+    };
+
+    formValidation = () => {
+        let username = false;
+        let email = false;
+        let pass = false;
+        let accept = false;
+        let correct = false;
+
+        if (this.state.username.length > 10 &&
+            this.state.username.indexOf(' ') === -1) {
+            username = true;
+        }
+
+        if (this.state.email.indexOf('@') !== -1) {
+            email = true;
+        }
+
+        if (this.state.pass.length === 8) {
+            pass = true;
+        }
+
+        if (this.state.accept) {
+            accept = true;
+        }
+
+        if (username && email && pass && accept) {
+            correct = true;
+        }
+
+        return ({
+            username,
+            email,
+            pass,
+            accept,
+            correct
+        })
+
     };
 
     render() {
@@ -52,33 +117,32 @@ class App extends Component {
                         <input type='text' id='user' name='username'
                                value={this.state.username} onChange={this.handleChange}/>
                     </label>
-                        {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
+                    {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
 
                     <label htmlFor='email'>Your email:
                         <input type='email' id='email' name='email'
                                value={this.state.email} onChange={this.handleChange}/>
                     </label>
-                        {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
+                    {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
 
                     <label htmlFor='pass'>Your password:
                         <input type='password' id='password' name='pass'
                                value={this.state.pass} onChange={this.handleChange}/>
                     </label>
-                        {this.state.errors.pass && <span>{this.messages.pass_incorrect}</span>}
+                    {this.state.errors.pass && <span>{this.messages.pass_incorrect}</span>}
 
                     <label htmlFor='accept'>
                         <input type='checkbox' id='accept' name='accept'
                                checked={this.state.accept}
                                onChange={this.handleChange}/>I accept the regulations and agree
                     </label>
-                        {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
+                    {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
 
                     <button>Submit</button>
                 </form>
             </div>
         );
     }
-
 }
 
 export default App;
